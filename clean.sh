8b3e603e086130782780e003f1725013b6a556e1
anderson.cody@gmail.com <ht2vm8UW7dY8>
2013-09-18 14:50:17 -0700

    Revert "Removed original ga-bitbot startup scripts and added my customized scripts.  NOTE: requires screen."
    This reverts commit 2f30dd8d489087a084731e6aa13fb0357723e8d9.

--- clean.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd datafeed
-sort -t ',' bcfeed_mtgoxUSD_1min.csv -o sorted && rm bcfeed_mtgoxUSD_1min.csv && mv sorted bcfeed_mtgoxUSD_1min.csv
-cd ..
--- stopMe.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-for i in `ps auxw|grep -i screen|grep -v grep | awk '{print $13}'` ; do screen -p 0 -S $i -X eval 'stuff ' ; sleep 1 ; screen -p 0 -S $i -X eval 'stuff \"exit\"\015' ; sleep 1 ; done
